4b8464603bf5872c2b246e9b5ffcbef1bf3c4769
Konrad Sygut <konrsyg825@student.polsl.pl>
2021-06-19 17:18:38 +0200

    poprawiony plik obluga .h na .cpp i .h

--- ConsoleApplication/Obsluga.cpp
+++ ConsoleApplication/Obsluga.cpp
@@ -1,3 +1,33 @@
 #include "Obsluga.h"
 
+int Obsluga::setMiesiac()
+{
+	if (rok > 1800 and rok < 1899)
+	{
+		miesiac -= 80;
+	}
+	else if (rok > 2000 and rok < 2099)
+	{
+		miesiac -= 20;
+	}
+	else if (rok > 2100 and rok < 2199)
+	{
+		miesiac -= 40;
+	}
+	else if (rok > 2200 and rok < 2299)
+	{
+		miesiac -= 60;
+	}
+	return miesiac;
+}
+
+void Obsluga::getMiesiac()
+{
+	cout << "Miesiac: " << miesiac << endl;
+}
+
+void Obsluga::data_urodzenia()
+{
+	cout << dzien << "." << miesiac << "." << rok << endl;
+}
 
--- ConsoleApplication/Obsluga.h
+++ ConsoleApplication/Obsluga.h
@@ -5,39 +5,14 @@ class Obsluga
 	:Data
 {
 public:
-	bool control;
+	
 
+	int setMiesiac();
 
-	int setMiesiac()
-	{
-		if (rok > 1800 and rok < 1899)
-		{
-			miesiac -= 80;
-		}
-		else if (rok > 2000 and rok < 2099)
-		{
-			miesiac -= 20;
-		}
-		else if (rok > 2100 and rok < 2199)
-		{
-			miesiac -= 40;
-		}
-		else if (rok > 2200 and rok < 2299)
-		{
-			miesiac -= 60;
-		}
-		return miesiac;
-	}
+	void getMiesiac();
 
-	void getMiesiac()
-	{
-		cout << "Miesiac: " << miesiac << endl;
-	}
-
-	void data_urodzenia()
-	{
-		cout << dzien << "." << miesiac << "." << rok << endl;
-	}
+	void data_urodzenia();
+	
 
 	Obsluga(Data obj)
 	{
